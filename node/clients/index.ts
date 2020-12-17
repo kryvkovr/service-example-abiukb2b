6f2e060de34d137e2f2097f6c0da3e6c25b2ba11
Volodymyr <kryvkovr@gmail.com>
2020-12-17 18:33:36 +0200

    add orders call

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,4 +1,5 @@
 import { IOClients } from '@vtex/api'
+import { Catalog, OMS } from '@vtex/clients'
 
 import Status from './status'
 
@@ -6,5 +7,13 @@ import Status from './status'
 export class Clients extends IOClients {
   public get status() {
     return this.getOrSet('status', Status)
+  }
+
+  public get catalog() {
+    return this.getOrSet('catalog', Catalog)
+  }
+
+  public get oms() {
+    return this.getOrSet('oms', OMS)
   }
 }
